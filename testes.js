2f2a548703cdb860aabc67bcb3932a66d3af8494
Refusado <orefusado@gmail.com>
2022-07-14 00:58:19 -0300

    exercício fibonacci

--- testes.js
+++ testes.js
@@ -1,42 +1,12 @@
-// const fs = require("fs");
-// const input = fs.readFileSync(0, "utf8");
+// a função gets é implementada dentro do sistema para ler as entradas(inputs) dos dados e a função print para imprimir a saída(output) de dados.
+// Abaixo segue um exemplo de código que você pode ou não utilizar
 
-var input = "4 1\n1 0\n0 4\n3 1\n2 3\n1 2\n2 0\n0 2\n1 2\n4 3\n0 1\n3 2\n3 4\n1 4\n1 0";
+let N = 4;
 
-const jogos = input.split('\n');
-const equipes = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P"];
-
-function result(j, t) {
-    for (let i = 1; i <= 4; i++) {
-        var timesRestantes = [];
-        var seVenceu = [];
-
-        j.map(function(value) {
-          var timesAtuais = value.split(' ');
-          
-          var time1 = timesAtuais[0];
-          var time2 = timesAtuais[1];
-          
-          if (time1 > time2) {
-            seVenceu.push(1);
-            seVenceu.push(0);
-          }
-          else {
-            seVenceu.push(0);
-            seVenceu.push(1);
-          }
-        });
-
-        for (let i = 0; i <= seVenceu.length / 2; i++) {
-          if (seVenceu[i]) {
-            timesRestantes.push(t[i]);
-          }
-        }
-
-        t = timesRestantes;
-        j.splice(0, timesRestantes.length);
-    }
-
-  return timesRestantes[0];
+//TODO: Complete os espaços em branco com uma possível solução para o desafio
+      
+for(let i = N - 1; i > 1; i--){
+  N = N * i;
 }
-console.log(result(jogos, equipes));
+      
+console.log(N);
