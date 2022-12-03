ba80d67908ae8b09e0efa1fa8bb7a0eedc295fa7
Renan <orefusado@gmail.com>
2022-12-03 05:37:34 -0300

    desafio duplicando zeros

--- duplicando-zeros/duplicar.js
+++ duplicando-zeros/duplicar.js
@@ -1,15 +1,14 @@
 let arr = [1, 0, 1, 0, 1];
+const originalLength = arr.length;
 
 // TODO: Retorne uma saída com os zeros devidamente duplicados
 
-console.log(arr);
-const originalLength = arr.length;
 for (let i = 0; i < originalLength; i++) {
   if (arr[i] == 0) {
     arr.splice(i + 1, 0, 0);
     i++;
   }
 }
-arr.length = originalLength;
 
+arr.length = originalLength;
 console.log(arr);
